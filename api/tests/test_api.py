d92928ecf17f6812e47b43715e0cd89b8e3c36b1
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-11-05 00:20:15 +0100

    cleanup

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -6,15 +6,12 @@ class TestAPIRoot:
     """Test cases for testing the / root of the API."""
 
     def _verify(self, endpoint: str):
-        response = requests.get(endpoint).json()
+        response = requests.get(endpoint, allow_redirects=True, headers = {'Accept': 'application/json'}).json()
 
         verify_as_json(response, options=Options().with_reporter(DiffReporter()))
 
     def test_root(self):
-        self._verify(f"http://localhost:8000/?format=json")
+        self._verify(f"http://localhost:8000/")
 
     def test_magic_missile(self):
-        self._verify(f"http://localhost:8000/v1/spells/magic-missile/?format=json")
-    
-
-
+        self._verify(f"http://localhost:8000/v1/spells/magic-missile/")
