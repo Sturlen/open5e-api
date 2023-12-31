9e8682b7bc6bc21302148dca2ed3dfe8289b9093
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-31 09:54:12 +0100

    linted

--- scripts/data_manipulation/lint_json.py
+++ scripts/data_manipulation/lint_json.py
@@ -2,19 +2,25 @@ import glob
 import json
 import os
 import sys
+import re
 
-root_path = os.environ['OPEN_5E_ROOT']
-sys.path.append(root_path)
-
-folder_location = root_path + ('' if root_path.endswith('/') else '/') + 'data/'
-
-filenames = glob.glob(folder_location + '**/*.json', recursive=True)
+filenames = glob.glob('./data/v1/**/Monster.json', recursive=True)
 
 for filename in filenames:
-    with open(filename, 'r') as f:
-        file = f.read()
+    try:
+        print(f'Linting {filename}')
+        with open(filename, 'r') as f:
+            file = f.read()
 
-    js = json.loads(file)
+        js = json.loads(file)
+        new_js = json.dumps(js, indent=2, ensure_ascii=False)
 
-    with open(filename, 'w') as f:
-        f.write(json.dumps(js, indent=4, ensure_ascii=False))
+        lines = new_js.split('\n')
+        lines = [re.sub(r'^  ','', line) for line in lines]  # Remove two spaces from the start of each line
+
+        new_js = '\n'.join(lines)
+        with open(filename, 'w') as f:
+            f.write(new_js)
+    except Exception as e:
+        print(f'Error linting {filename}')
+        print(e)
